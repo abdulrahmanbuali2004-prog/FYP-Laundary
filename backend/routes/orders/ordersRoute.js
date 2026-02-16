aca6eb4ed80cb5d6381c0b8acd7394a1a6b15775
Abdulrahman Buali <abdulrahmanbuali2004@gmail.com>
2026-02-16 18:49:39 +0300

    ordersRoute.js

---
+++ backend/routes/orders/ordersRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const secureRoute = require("../middleware/secureRoute");
+const orderController = require("../controllers/orderController");
+
+// Create a new order 
+router.post("/", secureRoute, orderController.createOrder);
+
+// Get orders 
+router.get("/", secureRoute, orderController.getAllOrders);
+
+// Get one order by id
+router.get("/:id", secureRoute, orderController.getOrderById);
+
+// Update order status 
+router.patch("/:id/status", secureRoute, orderController.updateOrderStatus);
+
+// Delete 
+router.delete("/:id", secureRoute, orderController.deleteOrder);
+
+module.exports = router;
