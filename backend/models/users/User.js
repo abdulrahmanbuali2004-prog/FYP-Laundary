812b0b74cd4543fd38c46fbf8520dd8189f82cfc
Abdulrahman Buali <abdulrahmanbuali2004@gmail.com>
2026-02-16 16:43:48 +0300

    fixed User model

--- backend/models/users/User.js
+++ backend/models/users/User.js
@@ -5,11 +5,31 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: {type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+
+  passwordHash: { 
+    type: String, 
+    required: true 
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "user", "driver"],
+    default: "user"
+  },
+
+  phone: { type: String },
+  address: { type: String },
+
+
 });
 
-// helper method to compare passwords
+// Compare passwords
 userSchema.methods.validatePassword = function (password) {
   return bcrypt.compare(password, this.passwordHash);
 };
